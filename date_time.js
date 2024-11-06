dbb46bd93b4722199de93de715ac2b4377a35c55
Devel-Anand <kulanthaianand4817@gmail.com>
2024-11-06 23:04:47 +0530

    Update date_time.js

--- date_time.js
+++ date_time.js
@@ -1,11 +1,7 @@
-//calculate different date between two dates 
-
 let startDate = '2024-11-01 18:30:00' // UTC
-
 let endDate = '2024-11-01 19:30:00' // UTC
 
 let timeStampResult = new Date(endDate).getTime() - new Date(startDate).getTime()
-
 
 let diffMins = Math.floor(timeStampResult/(1000*60))
 let diffHours = Math.floor(timeStampResult/(1000*60*60))
@@ -13,22 +9,10 @@ let diffDays = Math.floor(timeStampResult/(1000*60*60*24))
 let diffMonths =  Math.floor(timeStampResult/(1000*60*60*24*30))
 let diffYears = Math.floor(timeStampResult/(1000*60*60*24*30*12))
 
-let data = 2
-let oneDayMs = 1000*60*60*24  
-let addDate = new Date(startDate).getTime() + (oneDayMs * data)
-let oneMinuteMs = 1000*60
-let addMins = new Date(startDate).getTime() + (oneMinuteMs * data)
-
-
-
-console.log(new Date(addDate),"addDate")
-console.log(new Date(addMins),"addMins")
-
-
-console.log(diffDays,"diffDays")
-console.log(diffMonths,"diffMonths")
-console.log(diffYears,"diffYears")
-console.log(diffHours,"diffHours")
-console.log(diffMins,"diffMins  ")
+console.log(diffHours,"diffHours")  //O.P => 1
+console.log(diffMins,"diffMins") // O.P => 60 
+console.log(diffDays,"diffDays")      //O.P => 0 
+console.log(diffMonths,"diffMonths") // O.P => 0
+console.log(diffYears,"diffYears")   // O.P => 0 
 
 
