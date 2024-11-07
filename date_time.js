f87d8c00b60f8ef28c9f7248beae970db0923802
Devel-Anand <kulanthaianand4817@gmail.com>
2024-11-07 22:45:37 +0530

    Update date_time.js

--- date_time.js
+++ date_time.js
@@ -15,4 +15,18 @@ console.log(diffDays,"diffDays")      //O.P => 0
 console.log(diffMonths,"diffMonths") // O.P => 0
 console.log(diffYears,"diffYears")   // O.P => 0 
 
+//how to add hours and minutes Ex:I will going to add 5 hours and 30 minutes
+
+let addHours = new Date(startDate).getTime() + (1000*60*60*5)
+let addMins = addHours + (1000*60*30)
+let hoursAndMins = date_time_format(addMins) 
+console.log(hoursAndMins,"hoursAndMins") // O.P => 2024-10-02 00:00:00
+
+function date_time_format(formatDate){
+    let date = new Date(formatDate).getFullYear() + '-' +  (0 + new Date(formatDate).getMonth().toString()).slice(-2) + '-' + (0 + new Date(formatDate).getDate().toString()).slice(-2)
+    let time = (0 + new Date(formatDate).getHours().toString()).slice(-2) + ':' +  (0 + new Date(formatDate).getMinutes().toString()).slice(-2) + ':' + (0 + new Date(formatDate).getSeconds().toString()).slice(-2)
+    return date + " " + time
+}
+
+
 
